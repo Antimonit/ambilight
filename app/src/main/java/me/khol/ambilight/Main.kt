0c45e1901199bf0b8e43ca46d6b8027f02e0a443
David Khol <david@khol.me>
2019-05-01 13:33:50 +0200

    Updated main.

--- app/src/main/java/me/khol/ambilight/Main.kt
+++ app/src/main/java/me/khol/ambilight/Main.kt
@@ -1,3 +1,4 @@
+@file:JvmName("Main")
 package me.khol.ambilight
 
 import me.khol.ambilight.gui.ConfigFrame
@@ -7,52 +8,45 @@ import me.khol.ambilight.serial.SerialConnection
 import javax.swing.SwingUtilities
 import javax.swing.UIManager
 
-class Main private constructor() {
-
-	companion object {
-
-		@JvmStatic
-		fun main(args: Array<String>) {
-			try {
-				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
-			} catch (ignored: Exception) {
-			}
-			SwingUtilities.invokeLater { Main() }
-		}
+fun main() {
+	try {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+	} catch (ignored: Exception) {
 	}
+	SwingUtilities.invokeLater { start() }
+}
 
+fun start() {
 	// Configuration of our LED system
-	private val config = LedConfig()
+	val config = LedConfig()
 
 	// Bridge between Ambilight implementation and SerialConnection
-	private val connectionAdapter = ConnectionAdapter()
+	val connectionAdapter = ConnectionAdapter()
 
 	// GUI frame
-	private val window = ConfigFrame(config, connectionAdapter)
+	val window = ConfigFrame(config, connectionAdapter)
 
 	// Setup ambilight
-	private val ambilight: Ambilight = AmbilightGdi(
+	val ambilight: Ambilight = AmbilightGdi(
 		config.ledsWidth,
 		config.ledsHeight,
 		config.leds
 	)
 
 	// Serial connection that communicates with Arduino
-	private val connection: Connection = SerialConnection(config.ledCount)
+	val connection: Connection = SerialConnection(config.ledCount)
 
-	init {
-		connectionAdapter.setSerialConnection(connection)
-		connectionAdapter.open(Preferences.port)
+	connectionAdapter.setSerialConnection(connection)
+	connectionAdapter.open(Preferences.port)
 
-		// create a thread that repeatedly takes screenshots and returns colors back via a listener
-		val currentRunnable = LoopingRunnable(config, ambilight, SegmentColorsUpdateListener { colors ->
-			window.updatedSegmentColors(colors)
-			connection.sendColors(colors)
-		})
-		val currentThread = Thread(currentRunnable)
-		currentThread.start()
+	// create a thread that repeatedly takes screenshots and returns colors back via a listener
+	val currentRunnable = LoopingRunnable(config, ambilight, SegmentColorsUpdateListener { colors ->
+		window.updatedSegmentColors(colors)
+		connection.sendColors(colors)
+	})
+	val currentThread = Thread(currentRunnable)
+	currentThread.start()
 
-		// connect looping runnable and GUI
-		window.setGuiListener(currentRunnable)
-	}
+	// connect looping runnable and GUI
+	window.setGuiListener(currentRunnable)
 }
